a8091f267704109eece049e64f0a43e6cac5fb1b
lucca <luccavilasboas@hotmail.com>
2023-03-04 10:27:14 -0300

    Aula sabado

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
-    const [selectedItem, setSelectedItem] = useState('livro')
-
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -15,22 +12,18 @@ function NavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Menu</Nav.Link>
-                        <NavDropdown title={selectedItem} id="basic-nav-dropdown">
-                            <NavDropdown.Item href="livro" onSelect={() => setSelectedItem('livro')}>
-                                Livro
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="cliente" onSelect={() => setSelectedItem('cliente')}>
+                        <NavDropdown title="Cadastrar" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="livro">Livro</NavDropdown.Item>
+                            <NavDropdown.Item href="cliente">
                                 Cliente
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="aluguel" onSelect={() => setSelectedItem('aluguel')}>
-                                Aluguel
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="aluguel">Aluguel</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
